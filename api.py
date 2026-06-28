53d9fa587635edd95f5b9cee4e2627e01211455a
Aastha Pandey <aasthapandey8830@gmail.com>
2026-06-29 00:21:28 +0530

    Add CORS for frontend

--- api.py
+++ api.py
@@ -2,11 +2,28 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
+app.add_middleware(
 
+CORSMiddleware,
+
+allow_origins=[
+
+"http://localhost:3000"
+
+],
+
+allow_credentials=True,
+
+allow_methods=["*"],
+
+allow_headers=["*"],
+
+)
 
 # CORS FIX
 
